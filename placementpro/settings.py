03e9b3d4066baaa03f8772cea51342d8cd7c11ca
Himanshu <himanshubisht57172@gmail.com>
2026-04-26 01:17:58 +0530

    fix csrf issue

--- placementpro/settings.py
+++ placementpro/settings.py
@@ -17,7 +17,9 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = ["*"]
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.railway.app",
+]
 
 # Application definition
 
