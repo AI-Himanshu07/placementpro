a1dbecb5c46da175d2794b0c927c183e7a5a3f4d
Himanshu <himanshubisht57172@gmail.com>
2026-04-25 17:22:07 +0530

    add procfile for deployement

--- placementpro/settings.py
+++ placementpro/settings.py
@@ -64,12 +64,17 @@ WSGI_APPLICATION = 'placementpro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
-}
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
