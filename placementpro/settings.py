b23a5ec19138c023ffb3a3cd71e6a2040f5862a8
Himanshu <himanshubisht57172@gmail.com>
2026-04-25 16:57:57 +0530

    updated settings

--- placementpro/settings.py
+++ placementpro/settings.py
@@ -13,12 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '.onrender.com'
-]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
