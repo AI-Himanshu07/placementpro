63cc9916a771ae2bc3b308c61a5ed3d985d3c6f0
Himanshu <himanshubisht57172@gmail.com>
2026-04-26 08:58:47 +0530

    fix database config

--- placementpro/settings.py
+++ placementpro/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'placementpro.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default='sqlite:///db.sqlite3'
     )
 }
 # Password validation
