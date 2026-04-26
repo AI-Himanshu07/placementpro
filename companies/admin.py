5cdb467a5b887f5df499d9f5558b8d75fd83d0e0
Himanshu <himanshubisht57172@gmail.com>
2026-04-26 12:09:12 +0530

    fixing admin issue

--- companies/admin.py
+++ companies/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
-from .models import Company
+from .models import Company, Job, CompanyUser
 
-admin.site.register(Company)
+# ======================
+# COMPANY ADMIN
+# ======================
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'min_cgpa')
+    search_fields = ('name',)
+
+# ======================
+# JOB ADMIN (VERY IMPORTANT)
+# ======================
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company', 'min_cgpa', 'status')
+    list_filter = ('company',)
+    search_fields = ('title',)
+
+# ======================
+# COMPANY USER LINK
+# ======================
+@admin.register(CompanyUser)
+class CompanyUserAdmin(admin.ModelAdmin):
+    list_display = ('user', 'company')
