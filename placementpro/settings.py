10d564674f0b55e70b64132daaba6a3d3a6cb07b
Himanshu <himanshubisht57172@gmail.com>
2026-04-26 08:27:25 +0530

    add railway db

--- placementpro/settings.py
+++ placementpro/settings.py
@@ -76,16 +76,10 @@ WSGI_APPLICATION = 'placementpro.wsgi.application'
 
 
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT"),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3'
+    )
 }
 
 # Password validation
