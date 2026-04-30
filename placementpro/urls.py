54465031bd2120e3b33964c768f6280ae801311a
Himanshu <himanshubisht57172@gmail.com>
2026-04-30 22:57:42 +0530

    fixing issue

--- placementpro/urls.py
+++ placementpro/urls.py
@@ -46,4 +46,4 @@ urlpatterns = [
          ),
          name='password_reset_complete'),
 ]
-path(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)),
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
