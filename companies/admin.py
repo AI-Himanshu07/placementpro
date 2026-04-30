f00fc2eb880c318ea8e9ab503af5f9431b739b57
Himanshu <himanshubisht57172@gmail.com>
2026-04-30 21:12:53 +0530

    final ui fix bas + html

--- companies/admin.py
+++ companies/admin.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
 from .models import Company, Job
 
-# COMPANY ADMIN
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ('name', 'min_cgpa')
-    search_fields = ('name',)
 
-
-# JOB ADMIN
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     list_display = ('title', 'company', 'min_cgpa', 'status')
