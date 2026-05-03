d8a41747ad43e80a3819dad6ea56e154dcf352cb
Himanshu <himanshubisht57172@gmail.com>
2026-05-03 23:41:48 +0530

    fixing all isuues

--- placementpro/settings.py
+++ placementpro/settings.py
@@ -122,8 +122,8 @@ STATICFILES_DIRS = [
 ]
 
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/media/"
+STATIC_ROOT = os.path.join(BASE_DIR, "media")
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
